811fe4f08e8e1ee916b785c7e7ce28eaec9c92cf
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-08-09 15:35:39 +0530

    implemented home component layout with responsive css styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div>
       <Header />
+
+      <main className='main'>
+        <Home />
+      </main>
     </div>
   );
 }
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+        <a href='https://www.linkedin.com/in/prudhvi-raj06/' target="_blank" className='home__social-icon'>
+            <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href='https://github.com/prudv13' target="_blank" className='home__social-icon'>
+            <i class="uil uil-github-alt"></i>
+        </a>
+
+        <a href='https://dribbble.com/prudhvi13' target="_blank" className='home__social-icon'>
+            <i class="uil uil-dribbble"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social;
