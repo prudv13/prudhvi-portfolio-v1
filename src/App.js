91434c3f7bcb4d8ddc7894980304ea1e87ac3de1
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-08-11 16:30:31 +0530

    added contact compoent with emailjs dependency and responsive css styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/about/About';
+import Contact from './components/contact/Contact';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Qualification from './components/qualifications/Qualification';
@@ -16,6 +17,7 @@ function App() {
         <Skills />
         <Services />
         <Qualification />
+        <Contact />
       </main>
     </div>
   );
