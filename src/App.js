bb8e0f527ce8f0e24e49d68b452aa0bc3a1c7c08
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-08-11 12:29:18 +0530

    added qualitications component with responsive css styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/about/About';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Qualification from './components/qualifications/Qualification';
 import Services from './components/services/Services';
 import Skills from './components/skills/Skills';
 
@@ -14,6 +15,7 @@ function App() {
         <About />
         <Skills />
         <Services />
+        <Qualification />
       </main>
     </div>
   );
